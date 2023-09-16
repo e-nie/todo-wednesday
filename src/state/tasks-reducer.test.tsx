ac4da8a2014bb77e79cc8808741083cbc22c5704
Eva Niehaus <eniehaus111@gmail.com>
2023-09-17 00:42:06 +0200

    add lesson 09 tests

--- src/state/tasks-reducer.test.tsx
+++ src/state/tasks-reducer.test.tsx
@@ -1,5 +1,6 @@
 import {addTaskAC, changeTaskStatusAC, changeTaskTitleAC, removeTaskAC, tasksReducer} from './tasks-reducer'
 import { TasksStateType } from '../App'
+import {AddTodolistAC, RemoveTodolistAC} from "./todolists-reducer";
 
 test('correct task should be deleted from correct array', () => {
     const startState: TasksStateType = {
@@ -99,4 +100,76 @@ test('title of specified task should be changed', () => {
 
     expect(endState['todolistId2'][1].title).toBe('juice')
     expect(endState['todolistId1'][1].title).toBe('JS')
+})
+
+test('new array should be added when new todolist is added', () => {
+    const startState: TasksStateType = {
+        'todolistId1': [
+            {id: '1', title: 'CSS', isDone: false},
+            {id: '2', title: 'JS', isDone: true},
+            {id: '3', title: 'React', isDone: false}
+        ],
+        'todolistId2': [
+            {id: '1', title: 'bread', isDone: false},
+            {id: '2', title: 'milk', isDone: true},
+            {id: '3', title: 'tea', isDone: false}
+        ]
+    }
+
+    const action = AddTodolistAC('new todolist')
+
+    const endState = tasksReducer(startState, action)
+
+
+    const keys = Object.keys(endState)
+    const newKey = keys.find(k => k != 'todolistId1' && k != 'todolistId2')
+    if (!newKey) {
+        throw Error('new key should be added')
+    }
+
+
+    const obj = {
+        key1: 'value1',
+        key2: 'value2',
+        key3: 'value3'
+    };
+
+// Using the 'in' operator
+//     if ('key1' in obj) {
+//         console.log('key1 exists in obj');
+//     }
+
+// Using the 'hasOwnProperty' method
+//     if (obj.hasOwnProperty('key2')) {
+//         console.log('key2 exists in obj');
+//     }
+
+    expect(keys.length).toBe(3)
+    expect(endState[newKey]).toEqual([])
+})
+
+
+test('property with todolistId should be deleted', () => {
+    const startState: TasksStateType = {
+        'todolistId1': [
+            {id: '1', title: 'CSS', isDone: false},
+            {id: '2', title: 'JS', isDone: true},
+            {id: '3', title: 'React', isDone: false}
+        ],
+        'todolistId2': [
+            {id: '1', title: 'bread', isDone: false},
+            {id: '2', title: 'milk', isDone: true},
+            {id: '3', title: 'tea', isDone: false}
+        ]
+    }
+
+    const action = RemoveTodolistAC('todolistId2')
+
+    const endState = tasksReducer(startState, action)
+
+
+    const keys = Object.keys(endState)
+
+    expect(keys.length).toBe(1)
+    expect(endState['todolistId2']).not.toBeDefined()
 })
--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -1,5 +1,6 @@
 import {FilterValuesType, TasksStateType, TodolistType} from '../App';
 import {v1} from 'uuid';
+import {AddTodolistActionType, RemoveTodolistActionType} from "./todolists-reducer";
 
 export type RemoveTaskActionType = ReturnType<typeof removeTaskAC>
 export type AddTaskActionType = ReturnType<typeof addTaskAC>
@@ -7,7 +8,8 @@ export type changeTaskStatusActionType = ReturnType<typeof changeTaskStatusAC>
 export type changeTaskTitleActionType = ReturnType<typeof changeTaskTitleAC>
 
 
-type ActionsType = RemoveTaskActionType | AddTaskActionType | changeTaskStatusActionType|changeTaskTitleActionType |changeTaskTitleActionType
+type ActionsType = RemoveTaskActionType | AddTaskActionType | changeTaskStatusActionType | changeTaskTitleActionType |
+    changeTaskTitleActionType | AddTodolistActionType | RemoveTodolistActionType
 
 
 export const tasksReducer = (state: TasksStateType, action: ActionsType) => {
@@ -27,13 +29,23 @@ export const tasksReducer = (state: TasksStateType, action: ActionsType) => {
                 ...state,
                 [action.todolistId]: state[action.todolistId].map(t => t.id === action.taskId ? {...t, isDone: action.isDone} : t)
             }
-
-
         case 'CHANGE-TASK-TITLE':
             return {
                 ...state,
                 [action.todolistId]: state[action.todolistId].map(t => t.id === action.taskId ? {...t, title: action.title} : t)
             }
+        case 'ADD-TODOLIST':
+            return {
+                ...state,
+                [action.todolistId]: []
+            }
+        case 'REMOVE-TODOLIST': {
+            // let copyState = {...state}
+            // delete copyState[action.id]
+            // return copyState
+            const {[action.id]:[], ...rest} = state // удаление св-ва с помощью деструктуризации - при деструктуризации создается новый объект
+            return rest
+        }
 
         default:
             throw new Error("I don't understand this type")
